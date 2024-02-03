37965aaed9096b7f4bc12a076d41877d565d8fa7
Evan Lauer <lauerevan@gmail.com>
2024-02-03 17:14:49 -0600

    added csv

--- data_processing/process_ocean_velocity.py
+++ data_processing/process_ocean_velocity.py
@@ -2,7 +2,7 @@ import numpy as np
 import netCDF4 as nc
 import matplotlib.pyplot as plt
 
-dataset = nc.Dataset('./ECCO_ocean_bolus/ECCO_OCEAN_BOLUS_VELOCITY_SNIPPET.nc')
+dataset = nc.Dataset('./ECCO_ocean_velocity/ECCO_OCEAN_VELOCITY_SNIPPET.nc')
 
 for var_name, variable in dataset.variables.items():
     print(f"Variable: {var_name}")
